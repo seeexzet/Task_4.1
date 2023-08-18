a793b95b1340ed522a273e8d6bf6fa138723f3dc
Mikhail Kruglikov <mkruglicov@gmail.com>
2023-08-18 18:50:46 +0300

    Task 2

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+import csv
 
 
 def index(request):
@@ -9,6 +12,12 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
+
+    with open(settings.BUS_STATION_CSV) as f:
+        reader = csv.DictReader(f, fieldnames=None, restkey=None, restval=None, dialect='excel')
+        for row in reader:
+            print(row['first_name'], row['last_name'])
+
 
     context = {
     #     'bus_stations': ...,
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,8 +16,41 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
+
+def omlet(request):
+    servings = int(request.GET.get("servings",1))
+    omlet_data = DATA.get('omlet')
+    if servings != 1:
+        omlet_data.update({'яйца, шт': omlet_data.get('яйца, шт') * servings})
+        omlet_data.update({'молоко, л': omlet_data.get('молоко, л') * servings})
+        omlet_data.update({'соль, ч.л.': omlet_data.get('соль, ч.л.') * servings})
+
+    context = { 'recipe': omlet_data }
+    print(context)
+    return render(request, 'calculator/index.html', context)
+
+def pasta(request):
+    servings = int(request.GET.get("servings",1))
+    pasta_data = DATA.get('pasta')
+    if servings != 1:
+        pasta_data.update({'макароны, г': pasta_data.get('макароны, г') * servings})
+        pasta_data.update({'сыр, г': pasta_data.get('сыр, г') * servings})
+
+    context = {'recipe': pasta_data }
+    return render(request, 'calculator/index.html', context)
+
+def buter(request):
+    servings = int(request.GET.get("servings",1))
+    buter_data = DATA.get('buter')
+    if servings != 1:
+        buter_data.update({'хлеб, ломтик': buter_data.get('хлеб, ломтик') * servings})
+        buter_data.update({'колбаса, ломтик': buter_data.get('колбаса, ломтик') * servings})
+        buter_data.update({'сыр, ломтик': buter_data.get('сыр, ломтик') * servings})
+        buter_data.update({'помидор, ломтик': buter_data.get('помидор, ломтик') * servings})
+
+    context = {'recipe': buter_data }
+    return render(request, 'calculator/index.html', context)
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
